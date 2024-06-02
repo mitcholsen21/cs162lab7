fc0d9ed29c6a63ffd150087897df5fc514f3a80f
mitcholsen21 <molsen27@my.lanecc.edu>
2024-06-02 00:11:39 -0700

    added files

---
+++ StartingFiles/CustomerProductSolution/Product.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Xml.Serialization;
+
+namespace CustomerProductClasses
+{
+    [XmlType("Product")] // define Type
+    [XmlInclude(typeof(Clothing)), XmlInclude(typeof(Gear))]
+    // I added the keyword abstract.  Ignore the IComparable for now.
+    public abstract class Product : IComparable<Product>
+    {
+        private int id;
+        private string code;
+        private string description;
+        private decimal unitPrice;
+        private int quantity;
+
+        protected Product() { }
+
+        protected Product(int productId, string productCode, string desc, decimal price, int qty)
+        {
+            id = productId;
+            code = productCode;
+            description = desc;
+            unitPrice = price;
+            quantity = qty;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Code
+        {
+            get
+            {
+                return code;
+            }
+            set
+            {
+                code = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+            set
+            {
+                description = value;
+            }
+        }
+
+        public decimal UnitPrice
+        {
+            get
+            {
+                return unitPrice;
+            }
+            set
+            {
+                unitPrice = value;
+            }
+        }
+
+        public int QuantityOnHand
+        {
+            get
+            {
+                return quantity;
+            }
+            set
+            {
+                quantity = value;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null || obj.GetType() != this.GetType())
+                return false;
+            else
+            {
+                Product other = (Product)obj;
+                return other.Id == Id &&
+                    other.Code == Code &&
+                    other.Description == Description &&
+                    other.UnitPrice == UnitPrice &&
+                    other.QuantityOnHand == QuantityOnHand;
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return 13 + 7 * id.GetHashCode() +
+                7 * code.GetHashCode() +
+                7 * description.GetHashCode() +
+                7 * unitPrice.GetHashCode() +
+                7 * quantity.GetHashCode();
+        }
+
+        public static bool operator ==(Product p1, Product p2)
+        {
+            return p1.Equals(p2);
+        }
+
+        public static bool operator !=(Product p1, Product p2)
+        {
+            return !p1.Equals(p2);
+        }
+
+        #region Abstract Class changes
+
+        // This now includes the ShippingCharge ... even though I didn't write it yet!!!
+        // That's ok because I know it will be there for the other classes when I call it.
+        public override string ToString()
+        {
+            return String.Format("Id: {0} Code: {1} Description: {2} UnitPrice: {3:C} Quantity: {4} Shipping: {5:C}",
+                id, code, description, unitPrice, quantity, ShippingCharge);
+        }
+
+        // this FORCES classes that are derived from Product to write a read-only property ShippingCost
+        public abstract decimal ShippingCharge
+        {
+            get;
+        }
+        // I could have written this as a method instead of a property getter
+        // public abstract decimal CalculateShippingCost();
+
+        #endregion
+
+        #region Interface changes
+
+        // this is the required method for IComparable
+        // it compares 2 Products based on their code (I pulled that out of the air!)
+        // CompareTo returns 0 when the 2 products are == for sorting purposes
+        // a negative if this < other and a positive if this > other
+        public int CompareTo(Product other)
+        {
+            return String.Compare(this.code, other.code, StringComparison.OrdinalIgnoreCase);
+        }
+
+        #endregion
+    }
+}
---
+++ StartingFiles/CustomerProductSolution/ProductList.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerProductClasses
+{
+    // Nothing in this class has changed
+    public class ProductList : IEnumerable<Product>
+    {
+        // polymorphism - can contain product, clothing or gear objects
+        private List<Product> products;
+
+        public ProductList()
+        {
+            products = new List<Product>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return products.Count;
+            }
+        }
+
+        public void Fill()
+        {
+            products = ProductDB.GetProducts();
+        }
+
+        public void Save()
+        {
+            ProductDB.SaveProducts(products);
+        }
+
+        public void Sort()
+        {
+            products.Sort();
+        }
+
+        // polymorphism.  Can pass a product, clothing or gear object
+        public void Add(Product product)
+        {
+            products.Add(product);
+        }
+
+        // this method is probably not appropriate at this point.  All products have these 5 attributes.
+        // BUT clothing and gear have more.  Better to just add an object.
+        /*
+        public void Add(int id, string code, string description, decimal price, int quantity)
+        {
+            Product p = new Product(id, code, description, price, quantity);
+            products.Add(p);
+        }
+        */
+
+        // polymorphism.  Can pass a product, clothing or gear object
+        public void Remove(Product product)
+        {
+            products.Remove(product);
+        }
+
+        public override string ToString()
+        {
+            string output = "";
+            // polymorphism.  Can be a product, clothing or gear object
+            foreach (Product p in products)
+            {
+                // calls the correct version of ToString depending on the kind of object
+                output += p.ToString() + "\n";
+            }
+            return output;
+        }
+
+        // polymorphism.  Can return a product, clothing or gear object
+        public Product this[int i]
+        {
+            get
+            {
+                return products[i];
+            }
+            set
+            {
+                products[i] = value;
+            }
+        }
+
+        public Product this[string code]
+        {
+            get
+            {
+                foreach (Product p in products)
+                {
+                    if (p.Code == code)
+                        return p;
+                }
+                return null;
+            }
+        }
+
+        public static ProductList operator +(ProductList pl, Product p)
+        {
+            pl.Add(p);
+            return pl;
+        }
+
+        public static ProductList operator +(Product p, ProductList pl)
+        {
+            pl.Add(p);
+            return pl;
+        }
+
+        public static ProductList operator -(ProductList pl, Product p)
+        {
+            pl.Remove(p);
+            return pl;
+        }
+
+        public static ProductList operator -(ProductList pl, int count)
+        {
+            for (int i = 1; i <= count; i++)
+                pl.products.RemoveAt(0);
+            return pl;
+        }
+
+        #region Abstract Classes changes
+
+        // this is a new property.  It illustrates the use of the abstract property ShippingCharge
+        public decimal ShippingCharge
+        {
+            get
+            {
+                decimal total = 0;
+                foreach (Product p in products)
+                    // this won't work unless ALL classes implement shippingcharge
+                    total += p.ShippingCharge;
+
+                return total;
+
+            }
+        }
+
+        #endregion
+
+        #region Interface changes
+
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return ((IEnumerable<Product>)products).GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return ((IEnumerable<Product>)products).GetEnumerator();
+        }
+
+        #endregion
+    }
+}
